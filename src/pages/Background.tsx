e37ff6c6a3549f48590663d91f9df23f95105bce
cielestrial <32034862+cielestrial@users.noreply.github.com>
2023-01-21 05:05:39 -0500

    v1.2.1
    - timestep added for gameloop

--- src/pages/Background.tsx
+++ src/pages/Background.tsx
@@ -13,21 +13,26 @@ const Background = (props: propsType) => {
   const context = useContext(StateContext);
   const [hideCursor, setHideCursor] = useState(false);
   const timeout = useRef<NodeJS.Timeout>();
+  const timer = useRef<NodeJS.Timer>();
   const canRun = useRef(true);
   const waitTime = 25;
+  const targetFPS = 5;
+  const timestep = 1000 / targetFPS;
+
+  const mouse = useRef(document.getElementById("mouse-hitbox"));
+  const boundaries = useRef(mouse.current?.getBoundingClientRect());
 
   async function trackMouse(event: MouseEvent) {
     if (!canRun.current) return;
     canRun.current = false;
     timeout.current = setTimeout(() => {
-      const mouse = document.getElementById("mouse-hitbox");
-      const boundaries = mouse?.getBoundingClientRect();
+      mouse.current = document.getElementById("mouse-hitbox");
+      boundaries.current = mouse.current?.getBoundingClientRect();
 
-      if (mouse !== null && boundaries !== undefined) {
-        mouse.style.left = event.pageX + "px";
-        mouse.style.top = event.pageY + "px";
-
-        splatRaindrops(boundaries);
+      if (mouse.current !== null && boundaries.current !== undefined) {
+        mouse.current.style.left = event.pageX + "px";
+        mouse.current.style.top = event.pageY + "px";
+        boundaries.current = mouse.current?.getBoundingClientRect();
       }
       canRun.current = true;
     }, waitTime);
@@ -35,8 +40,12 @@ const Background = (props: propsType) => {
 
   useEffect(() => {
     document.addEventListener("mousemove", (event) => trackMouse(event));
+    timer.current = setInterval(() => {
+      if (boundaries.current !== undefined) splatRaindrops(boundaries.current);
+    }, timestep);
     return () => {
       document.removeEventListener("mousemove", (event) => trackMouse(event));
+      clearInterval(timer.current);
       clearTimeout(timeout.current);
     };
   }, []);
@@ -54,7 +63,7 @@ const Background = (props: propsType) => {
       <BsFillBucketFill
         id="mouse-hitbox"
         className={
-          "absolute fill-slate-400 w-fit h-fit text-8xl translate-x-[-50%] translate-y-[-50%] " +
+          "absolute fill-slate-400 w-fit h-fit text-7xl translate-x-[-50%] translate-y-[-50%] " +
           (!hideCursor ? "hidden " : "")
         }
       />
